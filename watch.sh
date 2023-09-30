bba591fcdb3b93dad7dc7816dc51389c2fe1c401
Patryk Stępień <fighting.dreamer.0@gmail.com>
2023-09-30 02:33:46 +0200

    Update watch.sh / ignore non "*.zig" files

--- watch.sh
+++ watch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 find exercises -name '*.zig' |
-  entr sh -c 'ls exercises -c | head -1 | cut -f 1 -d "_" | xargs -I "{}" zig build -Dn={}'
+  entr sh -c 'ls exercises -c | grep ".*\.zig" | head -1 | cut -f 1 -d "_" | xargs -I "{}" zig build -Dn={}'
